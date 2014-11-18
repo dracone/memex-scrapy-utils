15ef0e7b5e3aa6348ccd0c76783b9ca44f7b1c2a
Mateusz Golewski <shirk3y@gmail.com>
2014-11-18 01:31:10 +0100

    Lua directives support for Splash

--- scrapy_memex/downloadermiddleware/splash.py
+++ scrapy_memex/downloadermiddleware/splash.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-import logging
+
 import base64
-import copy
 import json
+import logging
+import os
+
 from urlparse import urljoin
+from urllib import urlencode
+
 from scrapy import log
+from scrapy.http import HtmlResponse
+from scrapy.utils.conf import closest_scrapy_cfg
 
 
 class SplashMiddleware(object):
@@ -33,66 +39,92 @@ class SplashMiddleware(object):
 
     """
     SPLASH_DEFAULT_URL = 'http://127.0.0.1:8050'
-    SPLASH_DEFAULT_PNG_DIR = 'screenshots'  # relative to <project dir>
+    SPLASH_DEFAULT_DIRECTIVES_DIR = 'directives'
     SPLASH_EXTRA_TIMEOUT = 30
-    SPLASH_DEFAULT_OPTIONS = {
-        'wait': '2.0',
-        'width': '1024',
-        'height': '768',
-        'timeout': '60',
-    }
-    SPLASH_RENDER_HTML_OPTIONS = {
-        'html': '1',
-    }
-    SPLASH_RENDER_PNG_OPTIONS = {
-        'png': '1'
-    }
     RESPECT_SLOTS = True
 
-    def __init__(self, crawler, splash_url, endpoint='render.json'):
+    def __init__(self, crawler, splash_url, directives_dir):
         self.crawler = crawler
-        self.splash_url = urljoin(splash_url, endpoint)
+        self.splash_url = splash_url
+        self.directives_dir = directives_dir
+        self._lua_cache = {}
 
     @classmethod
     def from_crawler(cls, crawler):
         url = crawler.settings.get('SPLASH_URL', cls.SPLASH_DEFAULT_URL)
-        return cls(crawler, url)
+        directives_dir = crawler.settings.get('SPLASH_DIRECTIVES_DIR',
+                                              cls.SPLASH_DEFAULT_DIRECTIVES_DIR)
+        project_root = os.path.dirname(closest_scrapy_cfg())
+        directives_dir = os.path.join(project_root, directives_dir)
+        return cls(crawler, url, directives_dir)
 
     def process_request(self, request, spider):
+        # Avoid recursion
         if request.meta.get('_splash'):
             return
-        # The most common usage is render HTML only, so lets accept this option
-        # and set most common options for the user.
-        # Callback will be called with rendered HTML, not with Splash JSON data
-        render_html = request.meta.get('splash_render_html')
-        # Return png in response.meta
-        render_png = request.meta.get('splash_render_png')
 
-        custom_splash_options = request.meta.get('splash', {})
-        splash_options = copy.deepcopy(self.SPLASH_DEFAULT_OPTIONS)
+        splash_options = request.meta.get('splash')
+        directive = request.meta.get('splash_directive', [])
 
-        if render_png:
-            # Can't store png only, because some response must be returned
-            render_html = True
-            splash_options.update(self.SPLASH_RENDER_PNG_OPTIONS)
-            splash_options.update(custom_splash_options)
+        if directive or splash_options:
+            if not self._is_request_type_supported(request):
+                return request
+            self._increase_stats()
+            # Decide which endpoint we'll use
+            if directive:
+                return self._lua_request(request, splash_options, directive)
+            elif splash_options:
+                return self._json_request(request, splash_options)
 
-        if render_html:
-            splash_options.update(self.SPLASH_RENDER_HTML_OPTIONS)
-            splash_options.update(custom_splash_options)
+    def _json_request(self, request, splash_options):
+        splash_url = urljoin(self.splash_url, 'render.json')
+        meta = self._prepare_meta(request, splash_options)
+        splash_options = self._prepare_splash_options(request, splash_options)
+        new_request = request.replace(
+            url=splash_url,
+            method='POST',
+            body=json.dumps(splash_options),
+            meta=meta,
+            headers={'Content-Type': 'application/json'},
+        )
+        return new_request
 
-        if not render_png and not render_html:
-            if custom_splash_options:
-                splash_options = custom_splash_options
-            else:
-                return
+    def _lua_request(self, request, splash_options, directive):
+        splash_url = urljoin(self.splash_url, 'render.lua')
+        lua_source = self._load_lua_source(directive)
+        meta = self._prepare_meta(request, splash_options)
+        splash_options['url'] = request.url
+        new_request = request.replace(
+            url=splash_url + '?' + urlencode(splash_options),
+            method='POST',
+            body=json.dumps({'lua_source': lua_source}),
+            meta=meta,
+            headers={'Content-Type': 'application/json'},
+        )
+        return new_request
 
+    def _load_lua_source(self, directive):
+        cached = self._lua_cache.get(directive)
+        if cached:
+            return cached
+        path = os.path.join(self.directives_dir, directive + '.lua')
+        with open(path) as f:
+            source = f.read()
+            self._lua_cache[directive] = source
+            return source
+
+    def _is_request_type_supported(self, request):
         if request.method != 'GET':
             log.msg("Only GET requests are supported by SplashMiddleware; "
                     "'%s' will be handled without Splash" %
                     request, logging.WARNING)
-            return request
+            return False
+        return True
 
+    def _increase_stats(self):
+        self.crawler.stats.inc_value('splash/request_count')
+
+    def _prepare_meta(self, request, splash_options):
         for key, value in splash_options.items():
             if key.lower() == 'timeout':
                 request.meta['download_timeout'] = max(
@@ -110,17 +142,12 @@ class SplashMiddleware(object):
             # delays and concurrency options.
             meta['download_slot'] = self._get_slot_key(request)
 
-        self.crawler.stats.inc_value('splash/request_count')
+        return meta
+
+    def _prepare_splash_options(self, request, splash_options):
         splash_options['url'] = request.url
         splash_options['headers'] = request.headers
-        req_rep = request.replace(
-            url=self.splash_url,
-            method='POST',
-            body=json.dumps(splash_options),
-            meta=meta,
-            headers={'Content-Type': 'application/json'},
-        )
-        return req_rep
+        return splash_options
 
     def process_response(self, request, response, spider):
         if '_splash' in request.meta:
@@ -128,17 +155,14 @@ class SplashMiddleware(object):
                                          response.status)
             if response.status != 200:
                 return response
-            if (request.meta.get('splash_render_html') and
-                    request.meta.get('splash_render_png')):
-                data = json.loads(response.body)
-                response = response.replace(
-                    body=data['html'].encode('utf-8'),
-                    encoding='utf-8',
-                    url=request.meta.pop('splash_target_url'),
-                )
+            data = json.loads(response.body)
+            response = HtmlResponse(
+                url=request.meta.pop('splash_target_url'),
+                body=data.get('html', u'').encode('utf-8'),
+                encoding='utf-8',
+            )
+            if 'png' in data:
                 request.meta['png'] = base64.b64decode(data['png'])
-            elif request.meta.get('splash_render_html'):
-                response = response.replace(url=request.url)
 
         return response
 
