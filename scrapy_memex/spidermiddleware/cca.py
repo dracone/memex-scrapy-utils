f4c77ec35f78e5415fca7544fbeeb0c4986b4e68
Mateusz Golewski <shirk3y@gmail.com>
2015-01-22 01:07:05 +0100

    Add CcaMiddleware to store request/responses in CCA format

---
+++ scrapy_memex/spidermiddleware/cca.py
@@ -0,0 +1,23 @@
+from scrapy_memex_api.convert import response2cca
+from scrapy.item import DictItem, Field
+
+
+class CcaMiddleware(object):
+
+    _item_class = None  # Cached dynamic CcaItem class
+
+    def process_spider_output(self, response, result, spider):
+        for r in result:
+            yield r
+        cca = response2cca(response, base64=True)
+        item = self.create_item(cca)
+        yield item
+
+    @classmethod
+    def create_item(cls, values):
+        if cls._item_class is None:
+            fields = {field_name: Field()
+                      for field_name in values.iterkeys()}
+            cls._item_class = type('CcaItem', (DictItem,), {'fields': fields})
+        item = cls._item_class(**values)
+        return item
